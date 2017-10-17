c71a9eba496ccddb843163a416cbea303996d5c3
Maho Misumi <maho.smile606@gmail.com>
2017-10-17 14:38:52 +0900

    修正

---
+++ ToDo3/ToDoViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ToDoViewController.swift
+//  ToDo3
+//
+//  Created by Maho Misumi on 2017/10/17.
+//  Copyright © 2017年 Maho Misumi. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet weak var todoTextField: UITextField!
+    
+    let saveDate : UserDefaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        todoTextField.text = saveDate.object(forKey: "todo") as? String
+        todoTextField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func add(_ sender: UIButton) {
+        saveDate.object(forKey: "todo")
+    }
+    
+    @IBAction func `return`(_ sender: UIBarButtonItem) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
