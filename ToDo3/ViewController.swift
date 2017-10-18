0d43aae01c985a84c53d093af7393db7a693c906
Maho Misumi <maho.smile606@gmail.com>
2017-10-18 19:53:13 +0900

    教科書のTableViewCellの書き方に変更

--- ToDo3/ViewController.swift
+++ ToDo3/ViewController.swift
@@ -9,29 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
-
+    
+    @IBOutlet weak var todoItemView: UITableView!
+    var todoItem = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         if UserDefaults.standard.object(forKey: "todo") != nil {
             todoItem = UserDefaults.standard.object(forKey: "todo") as! [String]
+            todoItemView.dataSource = self
         }
-            }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
-        cell.textLabel?.text = todoItem[indexPath.row]
-        return cell
     }
-    @IBOutlet weak var todoItemView: UITableView!
-    
-//    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
-//        if editingStyle == UITableViewCellEditingStyle.Delete{
-//            todoItem.removeAtIndex(indexPath.row)
-//            NSUserDefaults.standardUserDefaults().setObject(todoItem, forKey: "todoList")
-//            todolistTable.reloadData()
-//        }
-//    }
+//cellをEdit(編集)したい場合に使用する関数
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == UITableViewCellEditingStyle.delete{
             todoItem.remove(at: indexPath.row)
@@ -39,11 +28,11 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
             todoItemView.reloadData()
         }
     }
-
+//    テーブル（リスト）を下に引っ張って更新機能を記述します。
     override func viewDidAppear(_ animated: Bool) {
         todoItemView.reloadData()
     }
-
+//UserDefaultsの値が代入されている変数todoItemが保持している文字列の数をカウントします。
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todoItem.count
     }
@@ -53,6 +42,13 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    //ID付きのセルを取得して、セル付属のtextLabelに表示させる
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = todoItemView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = todoItem[indexPath.row]
+        return cell!
+    }
+    
 
 
 }
